99490998c7619c162130f49ee3ddf76f0486023b
Bauke Westendorp <baukewestendorp@gmail.com>
2024-04-08 01:40:09 +0200

    radiant: Remove old radiant code

--- crates/radiant/src/main.rs
+++ crates/radiant/src/main.rs
@@ -1,136 +1,40 @@
-use anyhow::Result;
-use assets::Assets;
-use backstage::command::Command;
-use gpui::{actions, App, AppContext, AssetSource, Global, KeyBinding, Menu, MenuItem};
-use theme::ThemeSettings;
-use ui::text_input;
+use std::path::PathBuf;
 
-use crate::workspace::action::{ExecuteCommand, SetCurrentCommand};
-use crate::workspace::Workspace;
+use app::run_app;
+use gpui::App;
 
-pub mod layout;
-pub mod workspace;
+mod app;
+mod assets;
+mod geometry;
+mod layout;
+mod output;
+mod showfile;
+mod window;
+mod workspace;
 
-actions!(app, [Quit]);
+use clap::Parser;
+
+/// Radiant is a lighting design software.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the showfile
+    #[arg(short, long)]
+    showfile: PathBuf,
+}
 
 fn main() {
     env_logger::init();
-    dotenv::dotenv().ok();
+    let args = Args::parse();
 
-    log::info!("Starting Radiant...");
+    let app = App::new();
+    let showfile_path = std::env::current_dir()
+        .expect("Failed to get current directory")
+        .join(args.showfile)
+        .canonicalize()
+        .unwrap()
+        .to_str()
+        .map(|p| p.to_string());
 
-    App::new().run(|cx: &mut AppContext| {
-        cx.set_global(ThemeSettings::default());
-        cx.set_global(AppState::default());
-
-        if let Err(err) = init_fonts(cx) {
-            log::error!("{}", err);
-        }
-
-        if let Err(err) = init_keybinds(cx) {
-            log::error!("{}", err);
-        }
-
-        init_menu(cx);
-
-        cx.on_action(quit);
-
-        cx.spawn(move |cx| async move {
-            match Workspace::new(&cx).await {
-                Err(err) => {
-                    log::error!("Failed to open workspace: {err}");
-                    cx.update(|cx| cx.quit()).unwrap();
-                }
-                Ok(mut workspace) => {
-                    workspace.start_dmx_output_loop(&cx).await;
-                }
-            }
-        })
-        .detach();
-
-        log::info!("Radiant initialized");
-    });
+    run_app(app, showfile_path)
 }
-
-fn init_fonts(cx: &mut AppContext) -> Result<()> {
-    cx.text_system().add_fonts(vec![
-        Assets.load("fonts/zed-sans/zed-sans-extended.ttf")?,
-        Assets.load("fonts/zed-sans/zed-sans-extendedbold.ttf")?,
-        Assets.load("fonts/zed-sans/zed-sans-extendeditalic.ttf")?,
-    ])
-}
-
-fn init_keybinds(cx: &mut AppContext) -> Result<()> {
-    // FIXME: Get the keybindings from a keybinds file.
-
-    set_default_keybinds(cx);
-    if cx.global::<AppState>().use_command_shortcuts {
-        set_command_shortcuts(cx);
-    }
-
-    cx.observe_global::<AppState>(|cx| {
-        if cx.global::<AppState>().use_command_shortcuts {
-            set_command_shortcuts(cx);
-        } else {
-            cx.clear_key_bindings();
-            set_default_keybinds(cx);
-        }
-    })
-    .detach();
-
-    log::info!("Initialized keybindings");
-
-    Ok(())
-}
-
-fn set_default_keybinds(cx: &mut AppContext) {
-    cx.bind_keys([
-        KeyBinding::new("cmd-q", Quit, None),
-        KeyBinding::new("escape", ExecuteCommand(Command::Clear), None),
-        // FIXME: This should be moved to the ui crate.
-        KeyBinding::new("enter", text_input::Enter, Some("TextInput")),
-        KeyBinding::new("backspace", text_input::Backspace, Some("TextInput")),
-        KeyBinding::new("delete", text_input::Delete, Some("TextInput")),
-    ]);
-
-    log::info!("Set default keybindings");
-}
-
-fn set_command_shortcuts(cx: &mut AppContext) {
-    cx.bind_keys([
-        KeyBinding::new("s", SetCurrentCommand(Some(Command::Store(None))), None),
-        KeyBinding::new("S", SetCurrentCommand(Some(Command::Select(None))), None),
-        KeyBinding::new("backspace", SetCurrentCommand(None), None),
-    ]);
-
-    log::info!("Set command shortcuts");
-}
-
-fn init_menu(cx: &mut AppContext) {
-    cx.set_menus(vec![Menu {
-        name: "Radiant",
-        items: vec![MenuItem::action("Quit", Quit)],
-    }]);
-
-    log::info!("Initialized menu");
-}
-
-fn quit(_: &Quit, cx: &mut AppContext) {
-    log::info!("Quitting Radiant...");
-    cx.quit();
-    log::info!("Quit Radiant");
-}
-
-pub struct AppState {
-    pub use_command_shortcuts: bool,
-}
-
-impl Default for AppState {
-    fn default() -> Self {
-        Self {
-            use_command_shortcuts: true,
-        }
-    }
-}
-
-impl Global for AppState {}
--- crates/radiant/src/workspace.rs
+++ crates/radiant/src/workspace.rs
@@ -1,78 +1,311 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::time::Duration;
+use gpui::prelude::FluentBuilder;
+use gpui::{
+    div, AppContext, Context, FocusHandle, FocusableView, Global, InteractiveElement, IntoElement,
+    ParentElement, Render, Styled, View, ViewContext, VisualContext, WindowContext,
+};
+use theme::ActiveTheme;
+use ui::button::{Button, ButtonStyle};
+use ui::selectable::Selectable;
+use ui::text_input::{self, TextInput};
 
-use anyhow::Result;
-use backstage::show::Show;
-use gpui::{AsyncAppContext, Context, Model, Task, Timer};
+use crate::app::AppState;
+use crate::layout::{LayoutView, GRID_SIZE};
+use crate::showfile::{Layout, ShowfileManager};
 
-use crate::layout::screen::Screen;
-use crate::layout::Layout;
+pub mod actions {
 
-pub mod action {
-    use backstage::command::Command;
+    use backstage::Command;
     use gpui::impl_actions;
 
-    impl_actions!(workspace, [ExecuteCommand, SetCurrentCommand]);
+    impl_actions!(app, [ExecuteCommand, SetCurrentCommand]);
 
-    #[derive(Clone, PartialEq, serde::Deserialize)]
+    #[derive(Debug, Clone, PartialEq, serde::Deserialize)]
     pub struct ExecuteCommand(pub Command);
 
-    #[derive(Clone, PartialEq, serde::Deserialize)]
+    #[derive(Debug, Clone, PartialEq, serde::Deserialize)]
     pub struct SetCurrentCommand(pub Option<Command>);
 }
 
-const DMX_OUTPUT_RATE: Duration = Duration::from_millis(1000 / 40);
-
 pub struct Workspace {
-    show: Model<Show>,
+    focus_handle: FocusHandle,
+    screen: View<Screen>,
 }
 
 impl Workspace {
-    pub fn new(cx: &AsyncAppContext) -> Task<Result<Self>> {
-        cx.spawn(move |mut cx| async move {
-            let show = get_show().await?;
-            let layout = get_layout()?;
-            let show_model = cx.new_model(|_cx| show)?;
-
-            let window_grid = cx.new_model(|_cx| layout.window_grid(1).unwrap().clone())?;
-
-            cx.update(|cx| {
-                Screen::open_window(show_model.clone(), window_grid, cx);
-            })?;
-
-            Ok(Self { show: show_model })
+    pub fn build(cx: &mut WindowContext) -> View<Self> {
+        cx.new_view(|cx| Self {
+            focus_handle: cx.focus_handle(),
+            screen: Screen::build(cx),
         })
     }
 
-    pub fn start_dmx_output_loop(&mut self, cx: &AsyncAppContext) -> Task<()> {
-        cx.spawn({
-            let show = self.show.clone();
-            |cx| async move {
-                loop {
-                    cx.update(|cx| {
-                        log::trace!("Outputting DMX data...");
-                        show.update(cx, |show, _cx| {
-                            show.recalculate_stage_output();
-                            show.send_stage_output_to_dmx_protocols();
-                        });
-                    })
-                    .unwrap();
+    fn handle_execute_command(
+        &mut self,
+        cmd: &actions::ExecuteCommand,
+        cx: &mut ViewContext<Self>,
+    ) {
+        if let Err(err) =
+            ShowfileManager::update(cx, |showfile, _cx| showfile.show.execute_command(&cmd.0))
+        {
+            log::error!("Failed to execute command '{}': {err}", cmd.0);
+        }
+        cx.notify();
+    }
 
-                    Timer::after(DMX_OUTPUT_RATE).await;
+    fn handle_set_current_command(
+        &mut self,
+        action: &actions::SetCurrentCommand,
+        cx: &mut ViewContext<Self>,
+    ) {
+        ShowfileManager::update(cx, |showfile, cx| {
+            showfile.show.current_command = action.0.clone();
+            cx.notify();
+        });
+    }
+}
+
+impl FocusableView for Workspace {
+    fn focus_handle(&self, _cx: &AppContext) -> FocusHandle {
+        self.focus_handle.clone()
+    }
+}
+
+impl Render for Workspace {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .key_context("Workspace")
+            .size_full()
+            .text_color(cx.theme().colors().text)
+            .font("Zed Sans")
+            .on_action(cx.listener(Self::handle_execute_command))
+            .on_action(cx.listener(Self::handle_set_current_command))
+            .track_focus(&self.focus_handle)
+            .child(self.screen.clone())
+    }
+}
+
+pub struct Screen {
+    current_layout_view: View<LayoutView>,
+    command_line: View<CommandLine>,
+}
+
+impl Screen {
+    pub fn build(cx: &mut WindowContext) -> View<Self> {
+        let current_layout_view = get_current_layout_view(cx);
+
+        cx.new_view(|cx| {
+            cx.observe_global::<ShowfileManager>({
+                move |screen: &mut Screen, cx| {
+                    screen.current_layout_view = get_current_layout_view(cx);
+                    cx.notify();
                 }
+            })
+            .detach();
+
+            Self {
+                current_layout_view,
+                command_line: CommandLine::build(cx),
             }
         })
     }
+
+    fn render_sidebar(&self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        let layouts = ShowfileManager::layouts(cx).clone();
+
+        // FIXME: For now we are showing 10 layouts, but this should be a
+        // inplace-scrollable, just like the pool windows.
+        let items = (1..=10).map(|id| {
+            let layout = layouts.layouts.iter().find(|layout| layout.id == id);
+            self.render_layout_item(layout, id, cx).into_any_element()
+        });
+
+        div().w_32().children(items)
+    }
+
+    fn render_layout_item(
+        &self,
+        layout: Option<&Layout>,
+        id: usize,
+        cx: &mut ViewContext<Self>,
+    ) -> impl IntoElement {
+        let border_color = match ShowfileManager::layouts(cx).selected_layout_id == id {
+            true => cx.theme().colors().border_selected,
+            false => match layout.is_some() {
+                true => cx.theme().colors().border,
+                false => cx.theme().colors().border_disabled,
+            },
+        };
+
+        let display = div()
+            .size_full()
+            .flex()
+            .justify_center()
+            .items_center()
+            .border_b()
+            .border_color(border_color)
+            .children(layout.map(|l| l.label.clone()));
+
+        let id_element = div()
+            .h_5()
+            .px_1()
+            .when(layout.is_none(), |this| {
+                this.text_color(cx.theme().colors().text_muted)
+            })
+            .child(id.to_string());
+
+        let content = div()
+            .flex()
+            .flex_col()
+            .h_full()
+            .child(display)
+            .child(id_element);
+
+        Button::new(ButtonStyle::Primary, id, cx)
+            .selected(ShowfileManager::layouts(cx).selected_layout_id == id)
+            .border_color(border_color)
+            .w_full()
+            .h(GRID_SIZE)
+            .text_sm()
+            .on_click(cx.listener({
+                let layout = layout.cloned();
+                move |_screen, _event, cx| {
+                    if let Some(layout) = &layout {
+                        ShowfileManager::update(cx, |showfile, _cx| {
+                            showfile.layouts.selected_layout_id = layout.id;
+                        });
+                        cx.notify();
+                    }
+                }
+            }))
+            .child(content)
+    }
 }
 
-async fn get_show() -> Result<Show> {
-    let file = File::open("example_show/show.json")?;
-    Show::from_file(file).await
+impl Render for Screen {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        let content = div()
+            .size_full()
+            .overflow_hidden()
+            .child(self.current_layout_view.clone());
+
+        div()
+            .size_full()
+            .flex()
+            .flex_col()
+            .child(
+                div()
+                    .size_full()
+                    .flex()
+                    .child(content)
+                    .child(self.render_sidebar(cx)),
+            )
+            .child(div().w_full().h_10().child(self.command_line.clone()))
+    }
 }
 
-fn get_layout() -> Result<Layout> {
-    let file = File::open("example_show/layout.json")?;
-    let reader = BufReader::new(file);
-    serde_json::from_reader(reader).map_err(Into::into)
+fn get_current_layout_view(cx: &mut WindowContext) -> View<LayoutView> {
+    let current_layout_model = cx.new_model(|cx| {
+        // FIXME: Handle nonexistent layout (this should not be possible, but lets
+        // softerror on this to be sure).
+        ShowfileManager::layouts(cx)
+            .layouts
+            .iter()
+            .find(|layout| layout.id == ShowfileManager::layouts(cx).selected_layout_id)
+            .unwrap()
+            .clone()
+    });
+
+    LayoutView::build(current_layout_model, cx)
+}
+
+pub struct CommandLine {
+    text_input: View<TextInput>,
+
+    focus_handle: FocusHandle,
+}
+
+impl CommandLine {
+    pub fn build(cx: &mut WindowContext) -> View<Self> {
+        cx.new_view(|cx| {
+            let focus_handle = cx.focus_handle();
+            let text_input =
+                cx.new_view(|cx| TextInput::new(None, "Command line", focus_handle.clone(), cx));
+
+            // Update the command input when the current command changes.
+            cx.observe_global::<ShowfileManager>(|command_line: &mut Self, cx| {
+                command_line.text_input.update(cx, |text_input, cx| {
+                    *text_input.text_mut() = ShowfileManager::show(cx)
+                        .current_command
+                        .map(|cmd| cmd.to_string())
+                        .unwrap_or_default();
+                    cx.notify();
+                });
+            })
+            .detach();
+
+            cx.subscribe(
+                &text_input,
+                |cmd_line: &mut CommandLine, _text_input, event, cx| match event {
+                    text_input::Event::Submit(input) => {
+                        cmd_line.handle_submit_command_input(input, cx)
+                    }
+                },
+            )
+            .detach();
+
+            cx.on_focus_in(&focus_handle, |_focus_handle, cx| {
+                AppState::update(cx, |app_state, _cx| app_state.use_command_shortcuts = false)
+            })
+            .detach();
+
+            cx.on_blur(&focus_handle, |_focus_handle, cx| {
+                AppState::update(cx, |app_state, _cx| app_state.use_command_shortcuts = true)
+            })
+            .detach();
+
+            Self {
+                text_input,
+                focus_handle,
+            }
+        })
+    }
+
+    fn handle_submit_command_input(&mut self, input: &str, cx: &mut WindowContext) {
+        if input.is_empty() {
+            return;
+        }
+
+        self.text_input.update(cx, |text_input, cx| {
+            text_input.clear(cx);
+            ShowfileManager::update(cx, |showfile, cx| {
+                if let Err(err) = showfile.show.execute_command_str(input) {
+                    log::error!("Failed to execute command: {}", err.to_string())
+                } else {
+                    cx.notify();
+                }
+            })
+        })
+    }
+}
+
+impl FocusableView for CommandLine {
+    fn focus_handle(&self, _cx: &AppContext) -> FocusHandle {
+        self.focus_handle.clone()
+    }
+}
+
+impl Render for CommandLine {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .size_full()
+            .border_t()
+            .border_color(cx.theme().colors().border)
+            .bg(cx.theme().colors().element_background)
+            .flex()
+            .flex_shrink()
+            .items_center()
+            .px_3()
+            .child(self.text_input.clone())
+            .track_focus(&self.focus_handle)
+    }
 }
