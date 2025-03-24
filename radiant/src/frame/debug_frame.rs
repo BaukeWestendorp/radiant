8f099fb72d9245250c24f14651ecd799385a559a
Bauke Westendorp <baukewestendorp@gmail.com>
2025-03-25 00:40:55 +0100

    Remove unused text and number field from DebugFrame

--- radiant/src/frame/debug_frame.rs
+++ radiant/src/frame/debug_frame.rs
@@ -1,39 +1,19 @@
 use flow::ProcessingContext;
 use gpui::*;
-use ui::{NumberField, TextField};
 
 use crate::effect_graph::EffectGraph;
 
 pub struct DebugFrame {
-    text_field: Entity<TextField>,
-    number_field: Entity<NumberField>,
-
     effect_graph: Entity<EffectGraph>,
 }
 
 impl DebugFrame {
     pub fn build(
         effect_graph: Entity<EffectGraph>,
-        window: &mut Window,
+        _window: &mut Window,
         cx: &mut App,
     ) -> Entity<Self> {
-        cx.new(|cx| {
-            let text_field = cx.new(|cx| {
-                let field = TextField::new("text_field_1", window, cx);
-                field.set_value("Text Field Value".into(), cx);
-                field
-            });
-
-            let number_field = cx.new(|cx| {
-                let mut field = NumberField::new("number_field_1", window, cx);
-                field.set_value(0.2, cx);
-                field.set_min(Some(0.0));
-                field.set_max(Some(1.0));
-                field
-            });
-
-            Self { text_field, number_field, effect_graph }
-        })
+        cx.new(|_cx| Self { effect_graph })
     }
 }
 
@@ -45,14 +25,6 @@ impl Render for DebugFrame {
             pcx.value
         });
 
-        div()
-            .size_full()
-            .flex()
-            .flex_col()
-            .gap_2()
-            .p_2()
-            .child(self.number_field.clone())
-            .child(self.text_field.clone())
-            .child(format!("graph value: {}", value))
+        div().size_full().flex().flex_col().gap_2().p_2().child(format!("graph value: {}", value))
     }
 }
