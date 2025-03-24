f14479d42cab68fc5b4901d655311da221cf71db
Bauke Westendorp <baukewestendorp@gmail.com>
2025-03-24 18:25:44 +0100

    Add `flow2` crate and convert `Socket` to `InputSocket` and `OutputSocket`

---
+++ flow2/src/lib.rs
@@ -0,0 +1,12 @@
+#[cfg(feature = "serde")]
+pub mod serde;
+
+pub(crate) mod graph;
+pub(crate) mod node;
+pub(crate) mod socket;
+pub(crate) mod template;
+
+pub use graph::*;
+pub use node::*;
+pub use socket::*;
+pub use template::*;
