4ea63f7e0738b3a766771226f74b50d6dde846ba
Bauke Westendorp <baukewestendorp@gmail.com>
2025-09-15 19:51:04 +0200

    headless: Use new `radiant` API

--- headless/src/app.rs
+++ headless/src/app.rs
@@ -4,7 +4,7 @@ use radiant::engine::Engine;
 
 /// Starts the app in headless mode.
 pub fn run(showfile_path: PathBuf) -> eyre::Result<()> {
-    let mut engine = Engine::new(showfile_path);
+    let mut engine = Engine::new(Some(showfile_path));
     engine.start()?;
     loop {}
 }
