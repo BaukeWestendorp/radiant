8bc75581ab61b5eec0ae91edf720b5cfcac3b219
Bauke Westendorp <baukewestendorp@gmail.com>
2025-09-17 17:23:00 +0200

    radiant: Add app menu

--- crates/radiant/src/app.rs
+++ crates/radiant/src/app.rs
@@ -1,10 +1,20 @@
 use std::path::PathBuf;
 
-use gpui::{App, Application};
+use gpui::{App, Application, Menu, MenuItem};
 
 use crate::engine::EngineManager;
 use crate::window;
 use crate::window::main::MainWindow;
+
+pub mod actions {
+    use gpui::{App, KeyBinding};
+
+    gpui::actions!(radiant, [Quit]);
+
+    pub(crate) fn init(cx: &mut App) {
+        cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
+    }
+}
 
 pub fn run(showfile_path: Option<PathBuf>) {
     Application::new().with_assets(ui::assets::Assets).run(move |cx: &mut App| {
@@ -13,7 +23,26 @@ pub fn run(showfile_path: Option<PathBuf>) {
         ui::init(cx).expect("failed to initialize ui crate");
         window::main::actions::init(cx);
 
+        actions::init(cx);
+        init_menus(cx);
+        init_actions(cx);
+
         EngineManager::init(showfile_path, cx).expect("failed to initialize AppState");
         MainWindow::open(cx);
     });
+}
+
+fn init_menus(cx: &mut App) {
+    cx.set_menus(vec![Menu {
+        name: "radiant".into(),
+        items: vec![MenuItem::Action {
+            name: "Quit".into(),
+            action: Box::new(actions::Quit),
+            os_action: None,
+        }],
+    }]);
+}
+
+fn init_actions(cx: &mut App) {
+    cx.on_action::<actions::Quit>(|_, cx| cx.quit())
 }
