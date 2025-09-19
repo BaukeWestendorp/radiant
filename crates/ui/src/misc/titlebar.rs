3b1102ea010ca5ad3b5f6bf548d36c2658047b30
Bauke Westendorp <baukewestendorp@gmail.com>
2025-09-19 14:13:27 +0200

    ui: Update titlebar with new traffic buttons

--- crates/ui/src/misc/titlebar.rs
+++ crates/ui/src/misc/titlebar.rs
@@ -3,11 +3,11 @@ use gpui::{App, Div, FontWeight, Pixels, Point, Stateful, Window, WindowControlA
 
 use crate::theme::ActiveTheme;
 
-pub const TRAFFIC_LIGHT_PADDING: Pixels = px(69.0);
-pub const TRAFFIC_LIGHT_POSITION: Point<Pixels> = Point::new(px(8.0), px(8.0));
+pub const TRAFFIC_LIGHT_WIDTH: Pixels = px(69.0);
+pub const TRAFFIC_LIGHT_POSITION: Point<Pixels> = Point::new(px(9.0), px(9.0));
 
 pub fn titlebar(window: &Window, cx: &App) -> Stateful<Div> {
-    let titlebar_height = px(33.0);
+    let titlebar_height = px(32.0);
 
     div()
         .id("titlebar")
@@ -15,14 +15,14 @@ pub fn titlebar(window: &Window, cx: &App) -> Stateful<Div> {
         .w_full()
         .min_h(titlebar_height)
         .max_h(titlebar_height)
-        .pl(TRAFFIC_LIGHT_PADDING)
+        .pl(TRAFFIC_LIGHT_WIDTH + TRAFFIC_LIGHT_POSITION.x)
         .pr(TRAFFIC_LIGHT_POSITION.x)
         .border_b_1()
         .border_color(cx.theme().title_bar_border)
         .bg(cx.theme().title_bar)
         .flex()
         .items_center()
-        .child(div().font_weight(FontWeight::BOLD).pb(px(-2.0)).child(window.window_title()))
+        .child(div().font_weight(FontWeight::BOLD).pb(px(-3.0)).child(window.window_title()))
         .on_click(|event, window, _| {
             if event.click_count() == 2 {
                 window.titlebar_double_click();
