d65596e44b29c66fcea518b66f34e1c7a2d08b5a
Bauke Westendorp <baukewestendorp@gmail.com>
2025-09-20 16:00:56 +0200

    nui: Add `Theme`

--- crates/nui/src/lib.rs
+++ crates/nui/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod assets;
 pub mod error;
+pub mod theme;
 pub mod wm;
 
 mod app_ext;
@@ -10,10 +11,13 @@ use eyre::ContextCompat;
 use gpui::App;
 
 use crate::error::Result;
+use crate::theme::Theme;
 use crate::wm::WindowManager;
 
 pub fn init(cx: &mut App) -> Result<()> {
     assets::load_fonts(cx).ok().wrap_err("failed to load fonts")?;
+
+    cx.set_global(Theme::default());
 
     let wm = WindowManager::new(cx);
     cx.set_global(wm);
