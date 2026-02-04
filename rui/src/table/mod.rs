222bd1cb996284d80bf412b385b121c069d31b29
Bauke Westendorp <baukewestendorp@gmail.com>
2026-02-04 21:53:04 +0100

    Remove ad-hoc resizing code

--- rui/src/table/mod.rs
+++ rui/src/table/mod.rs
@@ -1,8 +1,8 @@
 use std::ops::Range;
 
 use gpui::{
-    App, Div, DragMoveEvent, ElementId, EmptyView, Entity, EntityId, FontWeight,
-    ListSizingBehavior, Pixels, SharedString, Window, div, prelude::*, px, uniform_list,
+    App, Div, ElementId, Entity, FontWeight, ListSizingBehavior, Pixels, Window, div, prelude::*,
+    px, uniform_list,
 };
 
 mod column;
@@ -13,7 +13,7 @@ pub use column::*;
 pub use delegate::*;
 pub use state::*;
 
-use crate::{ActiveTheme, ElementExt, h_flex, theme::HslaExt};
+use crate::{ActiveTheme, h_flex, theme::HslaExt};
 
 pub(crate) mod action {
     use gpui::{App, actions};
@@ -56,16 +56,6 @@ impl<D: TableDelegate + 'static> RenderOnce for Table<D> {
             .bg(cx.theme().bg_primary)
             .child(header)
             .child(body)
-            .on_prepaint({
-                let state = self.state.clone();
-                move |bounds, _, cx| {
-                    state.update(cx, |state, cx| {
-                        state.table_bounds = Some(bounds);
-                        state.update_column_widths(cx);
-                        cx.notify();
-                    })
-                }
-            })
     }
 }
 
@@ -74,8 +64,8 @@ impl<D: TableDelegate + 'static> TableState<D> {
         window.line_height()
     }
 
-    fn render_cell(&self, col_ix: usize, _window: &mut Window, _cx: &mut Context<Self>) -> Div {
-        let column_width = self.column_bounds[col_ix].size.width;
+    fn render_cell(&self, _col_ix: usize, _window: &mut Window, _cx: &mut Context<Self>) -> Div {
+        let column_width = px(100.0);
         div().w(column_width).h_full().flex_shrink_0().overflow_hidden().whitespace_nowrap()
     }
 
@@ -89,23 +79,19 @@ impl<D: TableDelegate + 'static> TableState<D> {
         let column_label = delegate.column(col_ix, cx).label().to_owned();
         let column_count = delegate.column_count(cx) - 1;
 
-        h_flex()
-            .h_full()
-            .child(
-                self.render_cell(col_ix, window, cx)
-                    .id(ElementId::named_usize("th", col_ix))
-                    .bg(cx.theme().bg_secondary)
-                    .border_b_1()
-                    .border_color(cx.theme().border_primary)
-                    .hover(|e| e.bg(cx.theme().bg_secondary.hover()))
-                    .active(|e| e.bg(cx.theme().bg_secondary.active()))
-                    .px_1()
-                    .font_weight(FontWeight::BOLD)
-                    .child(column_label),
-            )
-            .children(
-                (col_ix != column_count).then(|| self.render_resize_handle(col_ix, window, cx)),
-            )
+        h_flex().h_full().child(
+            self.render_cell(col_ix, window, cx)
+                .id(ElementId::named_usize("th", col_ix))
+                .bg(cx.theme().bg_secondary)
+                .border_b_1()
+                .when(col_ix < column_count, |e| e.border_r_1())
+                .border_color(cx.theme().border_primary)
+                .hover(|e| e.bg(cx.theme().bg_secondary.hover()))
+                .active(|e| e.bg(cx.theme().bg_secondary.active()))
+                .px_1()
+                .font_weight(FontWeight::BOLD)
+                .child(column_label),
+        )
     }
 
     fn render_tr(
@@ -226,62 +212,5 @@ impl<D: TableDelegate + 'static> TableState<D> {
         .size_full()
         .with_sizing_behavior(ListSizingBehavior::Infer)
         .track_scroll(&self.vertical_scroll_handle)
-    }
-
-    fn render_resize_handle(
-        &self,
-        col_ix: usize,
-        _window: &mut Window,
-        cx: &mut Context<Self>,
-    ) -> impl IntoElement {
-        const HANDLE_SIZE: Pixels = px(4.0);
-
-        let group_id = SharedString::from(format!("resizable-handle:{}", col_ix));
-
-        h_flex()
-            .id(ElementId::named_usize("resizable-handle", col_ix))
-            .group(group_id.clone())
-            .occlude()
-            .cursor_col_resize()
-            .h_full()
-            .w(HANDLE_SIZE)
-            .ml(-(HANDLE_SIZE))
-            .justify_end()
-            .items_center()
-            .child(
-                div()
-                    .h_full()
-                    .justify_center()
-                    .bg(cx.theme().border_secondary)
-                    .group_hover(&group_id, |this| {
-                        this.bg(cx.theme().border_secondary.hover()).h_full()
-                    })
-                    .w(px(1.0)),
-            )
-            .on_drag_move(cx.listener({
-                let this_entity_id = cx.entity_id();
-                move |this, event: &DragMoveEvent<(EntityId, usize)>, _, cx| {
-                    let (entity_id, col_ix) = *event.drag(cx);
-
-                    if this_entity_id != entity_id {
-                        return;
-                    }
-
-                    let col_bounds = this.column_bounds[col_ix];
-
-                    this.resize_col(
-                        col_ix,
-                        event.event.position.x - HANDLE_SIZE - col_bounds.left(),
-                        cx,
-                    );
-
-                    cx.notify();
-                }
-            }))
-            .on_drag((cx.entity_id(), col_ix), |_, _, _, cx| {
-                cx.stop_propagation();
-                cx.new(|_| EmptyView)
-            })
-            .into_any_element()
     }
 }
