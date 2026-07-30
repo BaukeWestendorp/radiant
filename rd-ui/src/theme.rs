c8d78b4587d2851200f040996aa35cde99cc17a9
Bauke Westendorp <baukewestendorp@gmail.com>
2026-07-30 18:02:32 +0200

    rd-ui: dynamic accent colors in `Theme`

--- rd-ui/src/theme.rs
+++ rd-ui/src/theme.rs
@@ -18,12 +18,14 @@ impl ActiveTheme for App {
 
 pub trait HslaExt {
     fn disabled(&self) -> Hsla;
-
     fn hover(&self) -> Hsla;
-
     fn active(&self) -> Hsla;
-
     fn contrast(&self) -> Hsla;
+
+    fn with_h(&self, h: f32) -> Hsla;
+    fn with_s(&self, s: f32) -> Hsla;
+    fn with_l(&self, l: f32) -> Hsla;
+    fn with_a(&self, a: f32) -> Hsla;
 }
 
 impl HslaExt for Hsla {
@@ -57,6 +59,30 @@ impl HslaExt for Hsla {
         c.l = if c.l > 0.5 { 0.1 } else { 0.9 };
         c
     }
+
+    fn with_h(&self, h: f32) -> Hsla {
+        let mut c = *self;
+        c.h = h;
+        c
+    }
+
+    fn with_s(&self, s: f32) -> Hsla {
+        let mut c = *self;
+        c.s = s;
+        c
+    }
+
+    fn with_l(&self, l: f32) -> Hsla {
+        let mut c = *self;
+        c.l = l;
+        c
+    }
+
+    fn with_a(&self, a: f32) -> Hsla {
+        let mut c = *self;
+        c.a = a;
+        c
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -70,7 +96,6 @@ pub struct Theme {
     pub bg_secondary: Hsla,
     pub bg_tertiary: Hsla,
     pub bg_selected: Hsla,
-    pub bg_selected_extra: Hsla,
     pub bg_focus: Hsla,
     pub bg_table: Hsla,
     pub bg_table_odd: Hsla,
@@ -112,6 +137,8 @@ impl Theme {
     }
 
     pub fn light() -> Self {
+        let accent: Hsla = rgb(0xee5622).into();
+
         Self {
             font_size: px(14.0),
             radius: px(3.0),
@@ -121,29 +148,28 @@ impl Theme {
             bg_primary: rgb(0xffffff).into(),
             bg_secondary: rgb(0xf4f4f4).into(),
             bg_tertiary: rgb(0xeaeaea).into(),
-            bg_selected: hsla(0.561, 0.708, 0.89, 1.).into(),
-            bg_selected_extra: hsla(0.561, 0.313, 0.76, 1.).into(),
-            bg_focus: hsla(0.6, 0.608, 0.95, 1.).into(),
+            bg_selected: accent.with_s(0.708).with_l(0.89),
+            bg_focus: accent.with_s(0.608).with_l(0.95),
             bg_table: rgb(0xffffff).into(),
             bg_table_odd: rgb(0xf9f9f8).into(),
-            bg_tile_header: hsla(0.577, 0.386, 0.8, 1.).into(),
+            bg_tile_header: accent.with_s(0.386).with_l(0.8),
 
             fg_primary: hsla(0., 0., 0.07, 1.).into(),
             fg_secondary: hsla(0., 0., 0.3, 1.).into(),
             fg_tertiary: rgb(0x808080).into(),
-            fg_selected: hsla(0.6, 0.4, 0.1, 1.).into(),
-            fg_focus: hsla(0.562, 0.912, 0.15, 1.).into(),
-            fg_tile_header: hsla(0.554, 0.667, 0.1, 1.).into(),
+            fg_selected: accent.with_s(0.4).with_l(0.1),
+            fg_focus: accent.with_s(0.912).with_l(0.15),
+            fg_tile_header: accent.with_s(0.667).with_l(0.1),
 
             border_primary: hsla(0., 0., 0.84, 1.).into(),
             border_secondary: hsla(0., 0., 0.8, 1.).into(),
             border_tertiary: hsla(0., 0., 0.75, 1.).into(),
-            border_selected: hsla(0.561, 0.912, 0.4, 1.).into(),
-            border_focus: hsla(0.561, 0.912, 0.5, 1.).into(),
-            border_tile_header: hsla(0.571, 0.386, 0.725, 1.).into(),
+            border_selected: accent.with_s(0.912).with_l(0.4),
+            border_focus: accent.with_s(0.912).with_l(0.5),
+            border_tile_header: accent.with_s(0.386).with_l(0.725),
 
-            accent: rgb(0x3bb2f6).into(),
-            indicate: IndicationColors::light(),
+            accent,
+            indicate: IndicationColors::light(accent),
 
             contrast: rgb(0x000000).into(),
 
@@ -155,6 +181,8 @@ impl Theme {
     }
 
     pub fn dark() -> Self {
+        let accent: Hsla = rgb(0xee5622).into();
+
         Self {
             font_size: px(14.0),
             radius: px(3.0),
@@ -164,29 +192,28 @@ impl Theme {
             bg_primary: rgb(0x100f0f).into(),
             bg_secondary: rgb(0x1c1b1a).into(),
             bg_tertiary: rgb(0x302e2d).into(),
-            bg_selected: hsla(0.561, 0.313, 0.275, 1.),
-            bg_selected_extra: hsla(0.561, 0.213, 0.424, 1.),
-            bg_focus: rgb(0x153649).into(),
+            bg_selected: accent.with_s(0.513).with_l(0.275),
+            bg_focus: accent.with_s(0.55).with_l(0.18),
             bg_table: rgb(0x100f0f).into(),
             bg_table_odd: rgb(0x151414).into(),
-            bg_tile_header: rgb(0x1f3446).into(),
+            bg_tile_header: accent.with_s(0.38).with_l(0.20),
 
             fg_primary: rgb(0xebebeb).into(),
             fg_secondary: rgb(0xb3b3b3).into(),
             fg_tertiary: rgb(0x808080).into(),
-            fg_selected: rgb(0xbee5fc).into(),
-            fg_focus: rgb(0xbee5fc).into(),
-            fg_tile_header: rgb(0xdceff8).into(),
+            fg_selected: accent.with_s(0.90).with_l(0.86),
+            fg_focus: accent.with_s(0.90).with_l(0.86),
+            fg_tile_header: accent.with_s(0.61).with_l(0.92),
 
             border_primary: rgb(0x292929).into(),
             border_secondary: rgb(0x353535).into(),
             border_tertiary: rgb(0x404040).into(),
-            border_selected: rgb(0x3bb2f6).into(),
-            border_focus: rgb(0x3bb2f6).into(),
-            border_tile_header: rgb(0x2b4a61).into(),
+            border_selected: accent,
+            border_focus: accent,
+            border_tile_header: accent.with_s(0.38).with_l(0.27),
 
-            accent: rgb(0x3bb2f6).into(),
-            indicate: IndicationColors::dark(),
+            accent,
+            indicate: IndicationColors::dark(accent),
 
             contrast: rgb(0xffffff).into(),
 
@@ -215,7 +242,7 @@ pub struct IndicationColors {
 }
 
 impl IndicationColors {
-    pub fn light() -> Self {
+    pub fn light(accent: Hsla) -> Self {
         Self {
             danger: rgb(0xe12e2c).into(),
             warning: rgb(0xffc94d).into(),
@@ -224,18 +251,12 @@ impl IndicationColors {
         }
     }
 
-    pub fn dark() -> Self {
+    pub fn dark(accent: Hsla) -> Self {
         Self {
             danger: rgb(0xe12e2c).into(),
             warning: rgb(0xffc94d).into(),
             info: rgb(0x3bb2f6).into(),
             success: rgb(0x9ce152).into(),
         }
-    }
-}
-
-impl Default for IndicationColors {
-    fn default() -> Self {
-        Self::light()
     }
 }
